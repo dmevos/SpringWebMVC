2fc23737c2769a51bb0353b8409311dff5b6d2b4
Dmitry Osipov <deosipov@mail.ru>
2023-01-30 12:24:31 +0300

    Исправлены замечания в задании со звездочкой после проверки 2

--- src/main/java/ru/osipov/controller/PostController.java
+++ src/main/java/ru/osipov/controller/PostController.java
@@ -1,38 +1,34 @@
 package ru.osipov.controller;
 
 import org.springframework.web.bind.annotation.*;
-import ru.osipov.mapper.PostMapper;
 import ru.osipov.model.Post;
 import ru.osipov.dto.PostDTO;
 import ru.osipov.service.PostService;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
     private final PostService service;
-    private final PostMapper mapper;
 
-    public PostController(PostService service, PostMapper mapper) {
+    public PostController(PostService service) {
         this.service = service;
-        this.mapper = mapper;
     }
 
     @GetMapping
     public List<PostDTO> all() {
-        return service.all().stream().map(mapper::mapToPostDTO).collect(Collectors.toList());
+        return service.all();
     }
 
     @GetMapping("/{id}")
     public PostDTO getById(@PathVariable long id) {
-        return mapper.mapToPostDTO(service.getById(id));
+        return service.getById(id);
     }
 
     @PostMapping
     public PostDTO save(@RequestBody Post post) {
-        return mapper.mapToPostDTO(service.save(post));
+        return service.save(post);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/ru/osipov/service/PostService.java
+++ src/main/java/ru/osipov/service/PostService.java
@@ -1,33 +1,39 @@
 package ru.osipov.service;
 
 import org.springframework.stereotype.Service;
+import ru.osipov.dto.PostDTO;
 import ru.osipov.exception.NotFoundException;
+import ru.osipov.mapper.PostMapper;
 import ru.osipov.model.Post;
 import ru.osipov.repository.PostRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
-    // сервис завязан на интерфейс, а не на конкретную реализацию
     private final PostRepository repository;
+    private final PostMapper mapper;
 
-    public PostService(PostRepository repository) {
+    public PostService(PostRepository repository, PostMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
-    public List<Post> all() {
-        return repository.all();
+    public List<PostDTO> all() {
+        return repository.all().stream()
+                .map(mapper::mapToPostDTO)
+                .collect(Collectors.toList());
     }
 
-    public Post getById(long id) {
-        return repository.getById(id).orElseThrow(() ->
-                new NotFoundException("При попытке получить пост с id=" + id + " произошла исключительная ситуация"));
+    public PostDTO getById(long id) {
+        return mapper.mapToPostDTO(repository.getById(id).orElseThrow(() ->
+                new NotFoundException("При попытке получить пост с id=" + id + " произошла исключительная ситуация")));
     }
 
-    public Post save(Post post) {
-        return repository.save(post).orElseThrow(() ->
-                new NotFoundException("При попытке сохранить пост с id=" + post.getId() + " произошла исключительная ситуация"));
+    public PostDTO save(Post post) {
+        return mapper.mapToPostDTO(repository.save(post).orElseThrow(() ->
+                new NotFoundException("При попытке сохранить пост с id=" + post.getId() + " произошла исключительная ситуация")));
     }
 
     public void removeById(long id) {
